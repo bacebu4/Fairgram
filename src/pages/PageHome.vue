5c188adae72fa9f9aef31f88e74ae2f678bac079
bacebu4 <vasua14735@icloud.com>
2020-08-25 22:07:54 +0300

    made cards responsive

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,36 +1,56 @@
 <template>
   <q-page class="constrain q-pa-lg">
-    <q-card
-      class="card-post card--radiused q-mb-lg"
-      v-for="post in posts"
-      :key="post.id"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img
-              src="https://scontent-arn2-2.cdninstagram.com/v/t51.2885-19/s320x320/117433868_596686834347986_1746806507336062882_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com&_nc_ohc=jmXCf7MNAOUAX9dH-j5&oh=82fd867ea4f5a7ca25640385b91721ca&oe=5F6D70A1"
-            />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          class="card-post card--radiused q-mb-lg"
+          v-for="post in posts"
+          :key="post.id"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img
+                  src="https://scontent-arn2-2.cdninstagram.com/v/t51.2885-19/s320x320/117433868_596686834347986_1746806507336062882_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com&_nc_ohc=jmXCf7MNAOUAX9dH-j5&oh=82fd867ea4f5a7ca25640385b91721ca&oe=5F6D70A1"
+                />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">ui.bace</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">ui.bace</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
 
-      <q-img :src="post.imageUrl" />
+          <q-img :src="post.imageUrl" />
 
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
-      </q-card-section>
-    </q-card>
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img
+                src="https://scontent-arn2-2.cdninstagram.com/v/t51.2885-19/s320x320/117433868_596686834347986_1746806507336062882_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com&_nc_ohc=jmXCf7MNAOUAX9dH-j5&oh=82fd867ea4f5a7ca25640385b91721ca&oe=5F6D70A1"
+              />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">ui.bace</q-item-label>
+            <q-item-label caption>Vasilii Krasikov</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
