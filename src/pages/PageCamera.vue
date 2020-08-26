886ad73935b300cff0e7e3aebc20d27c4a6138ca
bacebu4 <vasua14735@icloud.com>
2020-08-26 07:23:18 +0300

    added user media with old browsers support

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,33 +1,46 @@
 <template>
-  <q-page class='constrain-more q-pa-lg'>
-    <div class='camera-frame q-pa-lg'>
-      <img src='https://cdn.quasar.dev/img/parallax2.jpg' alt class='full-width' />
+  <q-page class="constrain-more q-pa-lg">
+    <div class="camera-frame q-pa-lg">
+      <video ref="video" class="full-width" autoplay />
     </div>
 
-    <div class='text-center q-pa-lg'>
-      <q-btn round color='grey-10' size='lg' icon='eva-camera' />
+    <div class="text-center q-pa-lg">
+      <q-btn round color="grey-10" size="lg" icon="eva-camera" />
     </div>
 
-    <div class='row justify-center q-ma-md'>
-      <q-input class='col col-sm-10' outlined v-model='post.location' placeholder='Caption' dense />
+    <div class="row justify-center q-ma-md">
+      <q-input
+        class="col col-sm-10"
+        outlined
+        v-model="post.caption"
+        placeholder="Caption"
+        dense
+      />
     </div>
 
-    <div class='row justify-center q-ma-md'>
-      <q-input class='col col-sm-10' outlined v-model='post.caption' placeholder='Location' dense>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        class="col col-sm-10"
+        outlined
+        v-model="post.location"
+        placeholder="Location"
+        dense
+      >
         <template v-slot:append>
-          <q-btn round dense flat icon='eva-navigation-2-outline' />
+          <q-btn round dense flat icon="eva-navigation-2-outline" />
         </template>
       </q-input>
     </div>
 
-    <div class='row justify-center q-mt-lg'>
-      <q-btn unelevated rounded color='primary' no-caps label='Post image' />
+    <div class="row justify-center q-mt-lg">
+      <q-btn unelevated rounded color="primary" no-caps label="Post image" />
     </div>
   </q-page>
 </template>
 
 <script>
 import { uid } from "quasar";
+require("md-gum-polyfill");
 
 export default {
   name: "PageCamera",
@@ -38,10 +51,24 @@ export default {
         caption: "",
         location: "",
         photo: null,
-        date: Date.now(),
-      },
+        date: Date.now()
+      }
     };
   },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true
+        })
+        .then(stream => {
+          this.$refs.video.srcObject = stream;
+        });
+    }
+  },
+  mounted() {
+    this.initCamera();
+  }
 };
 </script>
 
