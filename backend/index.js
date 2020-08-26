c91135f961fc020c4a045dad26540f50911f8044
bacebu4 <vasua14735@icloud.com>
2020-08-26 21:32:38 +0300

    firebase connected

--- backend/index.js
+++ backend/index.js
@@ -2,34 +2,47 @@
  * Dependencies
  */
 
-express = require('express')
+express = require("express");
+const admin = require("firebase-admin");
 
 /**
  * config-express
  */
 
-const app = express()
+const app = express();
+
+/**
+ * config - firebase
+ */
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 /**
  * endpoint - posts
  */
 
-app.get('/posts', (request, response) => {
+app.get("/posts", (request, response) => {
   const posts = [
     {
-      caption: 'Golden Gate Bridge',
-      location: 'San Francisco'
+      caption: "Golden Gate Bridge",
+      location: "San Francisco"
     },
     {
-      caption: 'London Eye',
-      location: 'London'
-    },
-  ]
-  response.send(posts)
-})
+      caption: "London Eye",
+      location: "London"
+    }
+  ];
+  response.send(posts);
+});
 
 /**
  * listen
  */
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
