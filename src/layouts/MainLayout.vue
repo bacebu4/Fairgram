54f28588d3afccb2315147b6ac3068cba28a4b1e
bacebu4 <vasua14735@icloud.com>
2020-08-28 20:46:43 +0300

    mobile banner added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,39 +1,39 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-white text-grey-10" bordered>
-      <q-toolbar class="constrain">
-        <q-toolbar-title class="text-grand-hotel q-ml-md"
-          >Fairgram</q-toolbar-title
-        >
+  <q-layout view='lHh Lpr lFf'>
+    <q-header class='bg-white text-grey-10' bordered>
+      <q-toolbar class='constrain'>
+        <q-toolbar-title class='text-grand-hotel q-ml-md'>Fairgram</q-toolbar-title>
+        <q-btn flat round to='/' class='large-screen-only q-mr-md' icon='eva-home-outline' />
         <q-btn
           flat
           round
-          to="/"
-          class="large-screen-only q-mr-md"
-          icon="eva-home-outline"
-        />
-        <q-btn
-          flat
-          round
-          to="/camera"
-          class="large-screen-only q-mr-md"
-          icon="eva-plus-circle-outline"
+          to='/camera'
+          class='large-screen-only q-mr-md'
+          icon='eva-plus-circle-outline'
         />
       </q-toolbar>
     </q-header>
 
-    <q-footer class="bg-white small-screen-only" bordered>
-      <q-tabs
-        class="text-grey-10"
-        active-color="primary"
-        indicator-color="transparent"
-      >
-        <q-route-tab to="/" icon="eva-home-outline" />
-        <q-route-tab to="/camera" icon="eva-plus-circle-outline" />
+    <q-footer class='bg-white small-screen-only'>
+      <q-banner inline-actions class='bg-primary text-white' dense>
+        <template v-slot:avatar>
+          <q-avatar color='white' text-color='grey-10' icon='eva-camera-outline' font-size='16px' />
+        </template>
+        Install Fairgram?
+        <template v-slot:action>
+          <q-btn flat label='Yes' dense class='q-px-sm' />
+          <q-btn flat label='Later' dense class='q-px-sm' />
+          <q-btn flat label='Never' dense class='q-px-sm' />
+        </template>
+      </q-banner>
+
+      <q-tabs class='text-grey-10' active-color='primary' indicator-color='transparent'>
+        <q-route-tab to='/' icon='eva-home-outline' />
+        <q-route-tab to='/camera' icon='eva-plus-circle-outline' />
       </q-tabs>
     </q-footer>
 
-    <q-page-container class="bg-grey-2">
+    <q-page-container class='bg-grey-2'>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -44,7 +44,7 @@ export default {
   name: "MainLayout",
   data() {
     return {};
-  }
+  },
 };
 </script>
 
