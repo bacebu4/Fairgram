8450aebdf1d7c2d5a454126f2717788f00a0b1cd
bacebu4 <vasua14735@icloud.com>
2020-09-09 18:41:14 +0300

    added caching strategies

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -3,3 +3,47 @@
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+
+/*
+ * dependencies
+ */
+
+import {precacheAndRoute} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {StaleWhileRevalidate} from 'workbox-strategies';
+import {CacheFirst} from 'workbox-strategies';
+import {ExpirationPlugin} from 'workbox-expiration';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+
+
+/*
+* config
+*/
+
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+/*
+ * caching strategies
+ */
+
+//so hard because of no-cors mode
+registerRoute(
+  ({url}) => url.host.startsWith('fonts.g'),
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({url}) => url.href.startsWith('http'),
+  new StaleWhileRevalidate()
+);
